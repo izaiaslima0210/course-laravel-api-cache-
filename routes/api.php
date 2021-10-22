cda8f4f578e80ac07b10fae027650324e0c2c89f
Izaias Oliveira de Lima Júnior <45080516+izaiaslima0210@users.noreply.github.com>
2021-10-22 13:04:39 -0300

    update order route

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Api\ModuleController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/modules/{module}/lessons', LessonController::class);
-Route::apiResource('/courses/{course}/modules', ModuleController::class);
 
 Route::post('/courses', [CourseController::class, 'store']);
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{identify}', [CourseController::class, 'show']);
 Route::put('/courses/{course}', [CourseController::class, 'update']);
 Route::delete('/courses/{identify}', [CourseController::class, 'destroy']);
+
+Route::apiResource('/courses/{course}/modules', ModuleController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'ok']);
